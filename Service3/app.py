6f0b3bd3327048cf29866ed0c4055a7378ef8b21
myounis789 <mohammedyounis789@gmail.com>
2021-12-02 23:02:50 +0000

    CG-33 CG-34 CG-35 CG-36 #IN-PROGRESS Suit generator implemented and works as expected.

---
+++ Service3/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, Response
+import random
+
+app = Flask(__name__)
+
+@app.route('/suit')
+def suit_generator():
+    suits = ['Spades', 'Clubs', 'Hearts', 'Diamonds']
+    suit = random.choice(suits)
+    return Response(suit, mimetype="text/plain")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5002, debug=True)
