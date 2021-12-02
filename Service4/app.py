aa1eb558228dc0af27f234bbe31dc64090e8d385
myounis789 <mohammedyounis789@gmail.com>
2021-12-02 23:28:38 +0000

    Fixed formatting

--- Service4/app.py
+++ Service4/app.py
@@ -67,7 +67,7 @@ def generate_card():
     values = {'A':'Ace', '2':'Two', '3':'Three', '4':'Four', '5':'Five', '6':'Six', '7':'Seven', '8':'Eight', '9':'Nine', '10':'Ten', 'J':'Jack','Q':'Queen','K':'King'}
     value = values[symbol]
 
-    return jsonify({"value": value, "suit": suit, "image":card_image})
+    return jsonify({"value": value, "suit": suit, "image": card_image})
 
 
 
