1c0e13fe8614d87ae5ac9c6ca6853458eec00a87
myounis789 <mohammedyounis789@gmail.com>
2021-12-06 07:32:15 +0000

    Added xml junit to testing to view results on jenkins

--- tests.sh
+++ tests.sh
@@ -8,7 +8,7 @@ do
   python3 -m venv venv
   source venv/bin/activate
   pip3 install -r ../test_requirements.txt
-  python3 -m pytest --cov-report term-missing --cov=app
+  python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
   deactivate
   cd ..
 done
